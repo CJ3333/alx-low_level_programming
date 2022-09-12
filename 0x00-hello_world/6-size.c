727fb2477c036c152c1c14fc2d7a932844931475
Naomi Chelugo <chelugonaomi@gmail.com>
2022-09-12 11:35:43 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sizeof many var types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+
+	print f("Size of a char: %d byte(s)\n", sizeof (a));
+	print f("Size of an int: %d byte(s)\n", sizeof (b));
+	print f("Size of a long int: byte(s)\n", sizeof (c));
+	print f("Size of a long long int: byte(s)\n", sizeof (d));
+	print f("Size of a float: %d byte(s)\n, sizeof (e));
+	return (0);
+}
